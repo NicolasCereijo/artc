c936d15e3b066fd29ca020977c43f06985e4766e
Nico <nicolascereijoranchal@gmail.com>
2024-02-26 11:16:39 +0100

    Temporal test removed

--- core/__init__.py
+++ core/__init__.py
@@ -3,10 +3,6 @@ import core.artc_errors.validations.path as path_err
 
 def main():
     print("Harvesting potatoes, stay tuned")
-    path_err.validate_path("/home/nico/Música/Investigación/Colección sonidos investigación/Sonidos de agua/",
-                           "Air Woosh Underwater.mp3")
-    path_err.validate_path("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/",
-                           "bootstrap.min.css")
 
 
 if __name__ == "__main__":
