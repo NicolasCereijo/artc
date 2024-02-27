ee7c7e9acb2fc8e8857eb70a0021b2b4c994b569
Nico <nicolascereijoranchal@gmail.com>
2024-02-27 23:04:28 +0100

    Main temporal examples removed

--- core/__init__.py
+++ core/__init__.py
@@ -7,15 +7,7 @@ def main():
     print("========================== Starting the ARtC suite... ==========================")
 
     if os.access("artc_configurations/configurations.json", os.R_OK):
-        ejemplo = set.WorkingSet()
-        ejemplo.add_file("/home/nico/Música/Investigación/Colección sonidos investigación/Sonidos de agua/",
-                         "little-waves.mp3", configuration_path)
-        print(ejemplo.working_set)
-        ejemplo.remove_file("little-waves.mp3")
-        print(ejemplo.working_set)
-        ejemplo.add_directory("/home/nico/Música/Investigación/Colección sonidos investigación/Sonidos de agua/",
-                              configuration_path)
-        print(ejemplo.working_set)
+        print("Potato")
     else:
         print("========= Could not access configuration file, suite execution aborted =========\n"
               "The configurations.json file should be located in the /core/artc_configurations/\n"
