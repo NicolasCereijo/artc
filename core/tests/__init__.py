4f66387b4e2efd2de2dfab078573805875e1732f
Nico <nicolascereijoranchal@gmail.com>
2024-02-27 10:32:10 +0100

    Check configurations file before execute tests

--- core/tests/__init__.py
+++ core/tests/__init__.py
@@ -1,16 +1,20 @@
 from core.tests import test_configurations, test_errors
 import pytest
+import os
 
 
 def main():
     print("=================== Running the main test suite for ARtC... ====================")
 
-    result = pytest.main()
+    if os.access("../artc_configurations/configurations.json", os.R_OK):
+        result = pytest.main()
 
-    if result == 0:
-        print("========================== All tests ran successfully ==========================")
+        if result == 0:
+            print("========================== All tests ran successfully ==========================")
+        else:
+            print("=============== Bugs were found in the test set during execution ===============")
     else:
-        print("=============== Bugs were found in the test set during execution ===============")
+        print("======= Could not access configuration file, tests execution was aborted =======")
 
 
 if __name__ == "__main__":
