aeaf82601ac3ec5d84d473c72202f0f79708fb39
Nico <nicolascereijoranchal@gmail.com>
2024-03-18 19:23:42 +0100

    main example modified, ZCR

--- core/__init__.py
+++ core/__init__.py
@@ -17,10 +17,10 @@ def main():
         audio_signal_1 = example_set.__getitem__("little-waves.mp3")
         audio_signal_2 = example_set.__getitem__("waves-in-caves.wav")
 
-        print(f"ZCR de la señal 1 consigo misma: {analysis.compare_zcr(audio_signal_1, audio_signal_1)}")
-        print(f"ZCR de la señal 2 consigo misma: {analysis.compare_zcr(audio_signal_2, audio_signal_2)}")
-        print(f"ZCR entre las señales 1 y 2: {analysis.compare_zcr(audio_signal_1, audio_signal_2)}")
-        print(f"ZCR entre las señales 2 y 1: {analysis.compare_zcr(audio_signal_2, audio_signal_1)}")
+        print(f"ZCR of signal 1 with itself: {analysis.compare_zcr(audio_signal_1, audio_signal_1)}")
+        print(f"ZCR of signal 2 with itself: {analysis.compare_zcr(audio_signal_2, audio_signal_2)}")
+        print(f"ZCR between signals 1 and 2: {analysis.compare_zcr(audio_signal_1, audio_signal_2)}")
+        print(f"ZCR between signals 2 and 1: {analysis.compare_zcr(audio_signal_2, audio_signal_1)}")
     else:
         print("========= Could not access configuration file, suite execution aborted =========\n"
               "The configurations.json file should be located in the /core/artc_configurations/\n"
