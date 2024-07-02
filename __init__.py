83349d24675041975c4943cf0151247c2397f656
Nico <nicolascereijoranchal@gmail.com>
2024-07-02 21:20:10 +0200

    Launch refactor

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
+import core
 import sys
 import os
 
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root_dir)
+
+__all__ = ["core"]
--- __main__.py
+++ __main__.py
@@ -1,8 +1,28 @@
 import core
+import importlib.resources
+import logging
+import sys
+import os
 
 
 def main():
-    core.main()
+    configuration_path = str(importlib.resources.files('core.configurations') / 'default_configurations.json')
+    logger = core.errors.logger_config.LoggerSingleton().get_logger()
+
+    logger.info("Starting the ARtC suite...\n\n" +
+                "    |     '||''|.     .     ..|'''.|       .|'''.|            ||    .          \n" +
+                "   |||     ||   ||  .||.  .|'      '       ||..  '  ... ...  ...  .||.    .... \n" +
+                "  |  ||    ||''|'    ||   ||                ''|||.   ||  ||   ||   ||   .|...||\n" +
+                " .''''|.   ||   |.   ||   '|.      .      .     '||  ||  ||   ||   ||   ||     \n" +
+                ".|.  .||. .||.  '|' .||.   ''|....'       |'....|'   '|..'|. .||.  '|.'  '|...'\n")
+
+    if not os.access(configuration_path, os.R_OK):
+        logging.critical("Could not access configuration file, suite execution aborted. The\n"
+                         "default_configurations.json file should be located in the /core/configurations/\n"
+                         "folder. Check the directory and access permissions.")
+        sys.exit(1)
+
+    # core.tests.main()
 
 
 if __name__ == "__main__":
